66e0084c2a2779052d5391bbdf1848aa9d76cd15
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-04-02 20:49:04 -0400

    Initial Commit

---
+++ Art Generator 2/AppEntry.swift
@@ -0,0 +1,20 @@
+//
+//  Art_Generator_2App.swift
+//  Art Generator 2
+//
+//  Created by Leonard McCook-Carr on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onAppear {
+                    print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+                }
+        }
+    }
+}
