dc504e70c5883fbdfb90dd248333f36f77f4dcac
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-04-15 01:41:53 -0400

    Modify the DALLEImagesView textfield

--- Art Generator 2/Screens/DALLE Images/DALLEImagesView.swift
+++ Art Generator 2/Screens/DALLE Images/DALLEImagesView.swift
@@ -41,8 +41,8 @@ struct DALLEImagesView: View {
                             .frame(width: 256, height: 256)
                     }
                     if vm.urls.isEmpty {
-                        Text("The more descriptive you can be, the better")
-                        TextField("Image Description....",
+                        Text("The more descriptive you are, the better")
+                        TextField("Image Description...",
                                   text: $vm.prompt,
                                   axis: .vertical)
                         .textFieldStyle(.roundedBorder)
